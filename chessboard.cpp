630c0d8b4352f09d6d5799990e5b30ba58f205d0
MUKUL BANSAL <curlvector747@gmail.com>
2021-12-21 16:42:05 +0530

    Create chessboard.cpp

---
+++ chessboard.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	int totalboxes=n*n;
+	int blackcells=totalboxes/2;
+	cout<< blackcells<<endl;
+	return 0;
+}
