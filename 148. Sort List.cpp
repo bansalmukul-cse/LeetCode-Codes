d2baf494d9878f40960787a870e084e1818a222b
MUKUL BANSAL <curlvector747@gmail.com>
2022-03-02 09:42:38 +0530

    Create 148. Sort List.cpp

---
+++ 148. Sort List.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergesort(ListNode* l1, ListNode* l2){
+        ListNode* result = NULL;
+        if(l1==NULL)
+            return l2;
+        if(l2==NULL)
+            return l1;
+        if(l1->val <=l2->val){
+            l1->next = mergesort(l1->next,l2);
+            result=  l1;
+        }
+        else{
+            l2->next = mergesort(l1,l2->next);
+            result =  l2;
+        }
+        return(result);
+       
+    }
+    
+    ListNode* sortList(ListNode* head) {
+        if(head == NULL || head->next==NULL)
+            return head;
+        
+        // Finding the middle point first
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        
+        while(fast!=NULL && fast->next!=NULL){
+            slow = slow->next;
+            fast =fast->next->next;  
+        }
+        // creating two seperate Linked list l1,l2
+        ListNode* newhead = slow->next;
+        slow->next= NULL;
+        
+        return mergesort(sortList(newhead),sortList(head));
+        
+    }
+};
