77d624bb264da551980a1b40bfbe03ca05c1d519
MUKUL BANSAL <curlvector747@gmail.com>
2022-03-30 08:38:38 +0530

    Create 1249. Minimum Remove to Make Valid Parentheses.cpp

---
+++ 1249. Minimum Remove to Make Valid Parentheses.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+       stack<int>st;
+        string res ="";
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='(')
+                st.push(i);
+            else if(s[i]==')'){
+                if(!st.empty())
+                    st.pop();
+                else
+                    s[i]='#';
+            }
+        }
+        while(!st.empty()){
+            s[st.top()] = '#';
+            st.pop();
+            
+        }
+        for (int i = 0; i < s.length(); ++i)
+        {
+            if (s[i] != '#')
+                res.push_back(s[i]);
+        }
+
+        return res;
+    }
+};
