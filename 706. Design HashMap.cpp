6c42c813ce3e1cabf22b217e8b0021dca96eb8a1
MUKUL BANSAL <curlvector747@gmail.com>
2022-05-29 17:08:13 +0530

    Create 706. Design HashMap.cpp

---
+++ 706. Design HashMap.cpp
@@ -0,0 +1,50 @@
+class MyHashMap {
+    vector<list<pair<int,int>>> map;
+    size_t m_size = 10000;
+
+public:
+    
+    MyHashMap() {
+        map.resize(m_size);
+    }
+    
+    void put(int key, int value) {
+        auto &list = map[key % m_size];
+        for (auto & val : list) {
+            if (val.first == key) {
+                val.second = value;
+                return;
+            }
+        }
+        list.emplace_back(key, value);
+    }
+    
+    int get(int key) {
+        const auto &list = map[key % m_size];
+        if (list.empty()) {
+            return -1;
+        }
+        for (const auto & val : list) {
+            if (val.first == key) {
+                return val.second;
+            }
+        }
+        return -1;
+    }
+    
+    void remove(int key) {
+        auto& pairs = map[key%m_size];
+        for(auto i=pairs.begin(); i!= pairs.end(); i++) {
+            if(i->first==key) { 
+                pairs.erase(i); return; 
+            }
+        }
+    }
+};
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
